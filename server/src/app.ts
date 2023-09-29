b5254709af3dd71987632ad6738097753226d659
Tung Tran Ngoc <tung.tran@wesport.zone>
2023-09-30 00:06:08 +0700

    using middleware, helmet.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import compressFilter from './utils/compressFilter.util';
+import config from './config/config';
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+const app: Express = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(
+  cors({
+    // origin is given a array if we want to have multiple origins later
+    origin: [config.cors_origin],
+    credentials: true,
+  })
+);
+
+// Helmet is used to secure this app by configuring the http-header
+app.use(helmet());
+
+// Compression is used to reduce the size of the response body
+app.use(compression({ filter: compressFilter }));
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
