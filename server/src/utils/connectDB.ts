da91fddd7f01caf03e40cb2d0a27889e4db49316
Tung Tran Ngoc <tung.tran@wesport.zone>
2023-09-30 21:52:34 +0700

    Add Connect Database Utils

---
+++ server/src/utils/connectDB.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'; 
+import logger from 'src/middleware/logger';
+const dbUrl = `mongodb://${process.env['MONGODB_USERNAME']}:${ process.env['MONGODB_PASSWORD']}@${process.env['MONGODB_IP_ADDRESS']}:${process.env['MONGODB_DATABASE_PORT']}/${process.env['MONGODB_DATABASE_NAME']}?authSource=admin`;
+
+let urlConnect = process.env['MONGODB_URI']? process.env['MONGODB_URI'] : dbUrl;
+const connectDB = async () => {
+  try {
+    
+    logger.info('Database connect to...' +(urlConnect));
+    await mongoose.connect(urlConnect );
+    logger.info('Database connected...');
+  } catch (error: any) {
+    logger.error(error.message);
+    logger.error('Database connect error...' +(urlConnect));
+    setTimeout(connectDB, 5000);
+  }
+};
+
+export default connectDB;
