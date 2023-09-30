474eaff4d1d2ffc127369599901a6999afc09f62
Tung Tran Ngoc <tung.tran@wesport.zone>
2023-09-30 21:47:06 +0700

    handle Unknown Router and Error Handler
    Handler Unknown Router
    Error Handler
    Add Logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import compressFilter from './utils/compressFilter.util';
 import config from './config/config';
+import logger from './middleware/logger';
 
 const app: Express = express();
+
+// 1. Body Parser
+app.use(express.json({ limit: '9kb' }));
 
 app.use(
   cors({
@@ -21,8 +25,31 @@ app.use(helmet());
 // Compression is used to reduce the size of the response body
 app.use(compression({ filter: compressFilter }));
 
+app.use(express.static('public'))
+
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+
+// UnKnown Routes
+app.all('*', (req: Request, _: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  logger.error(`Route ${req.originalUrl} not found`);
+  err.statusCode = 404;
+  next(err);
+});
+
+// Global Error Handler
+app.use((err: any, _: Request, res: Response, _next: NextFunction) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+   
+  logger.error(`Error Handler ${err.statusCode}`);
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 export default app;
