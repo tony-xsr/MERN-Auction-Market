2733bf910b90e3f5fba49a457a3dc69ded67e744
Tung Tran Ngoc <tung.tran@wesport.zone>
2023-10-03 22:59:08 +0700

    Remove unuse code

--- client/src/components/AuctionsFeed/index.tsx
+++ client/src/components/AuctionsFeed/index.tsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import './styles.css'; 
 import OngoingAuctionItem from "../OngoingAuctionItem";
 import { useUser } from "../../context/UserContext";
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 
@@ -38,24 +37,12 @@ const AuctionFeed = () => {
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         { auctions && auctions.length>0 ?   
                [...auctions].reverse().map((auction: any, index) => ( 
-                  //  <Grid item xs={2} sm={4} md={4} key={index}>
                      <OngoingAuctionItem auction={auction} key={auction._id} joinAuction={joinAuction}/> 
-                  //  </Grid>
                 ) ) : <div>No Auction Found</div>
          }
-        {/* {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <div>xs=2</div>
-            </Grid>
-          ))} */}
+       
         </Grid>
-        {/* <div style={{backgroundColor:'red'}} className="my-auctions auction-grid">
-            { auctions && auctions.length>0 ?   
-               [...auctions].reverse().map((auction: any, index) => ( 
-                   <OngoingAuctionItem auction={auction} key={auction._id} joinAuction={joinAuction}/> 
-                ) ) : <div>No Auction Found</div>
-         }
-        </div> */}
+ 
       </div>
     );
   };
