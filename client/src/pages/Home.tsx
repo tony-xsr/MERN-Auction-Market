ce2ff4359e6da7575fae0ca8465473c420940799
Tung Tran Ngoc <tung.tran@wesport.zone>
2023-10-03 22:55:49 +0700

    adding component AuctionFeed in home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 // Home.tsx
 import React from 'react';
 import MyAuctions from '../components/MyAuctions';
+import AuctionFeed from '../components/AuctionsFeed';
 
 const Home: React.FC = () => {
   return (  
       <div className='home'>
         <div className="container">
         <div className="left-column"> 
+          <AuctionFeed />
         </div>
         <div className="right-column">
             <MyAuctions />
