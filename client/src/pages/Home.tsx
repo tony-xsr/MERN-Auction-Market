085bcc90497a8f079b1ca5a906adcb263dd3e9d5
Tung Tran Ngoc <tung.tran@wesport.zone>
2023-10-04 16:47:24 +0700

    fix homepage layout

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,15 +2,19 @@
 import React from 'react';
 import MyAuctions from '../components/MyAuctions';
 import AuctionFeed from '../components/AuctionsFeed';
+import UserInfo from '../components/UserInfo';
 
 const Home: React.FC = () => {
   return (  
       <div className='home'>
-        <div className="container">
-        <div className="left-column"> 
-          <AuctionFeed />
+        <div className="container" style={{display:'flex',flexDirection:'row', flex:3}}>
+        <div className="left-column"  > 
+          <AuctionFeed />  
         </div>
-        <div className="right-column">
+        <div className="right-column" style={{flex:1 ,backgroundColor:'#e0e0e0',flexDirection:'column', marginTop:50}}>
+            <div style={{flex:1, backgroundColor:'#e0e0e0', height:300,  width:'100%', marginTop:350 }}>
+              <UserInfo /> 
+            </div>
             <MyAuctions />
         </div>
         </div>
